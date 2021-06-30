3b9b47682108d1c5cb6a0d84fbb5fa2e744699ec
AmirNogerov <86724696+AmirNogerov@users.noreply.github.com>
2021-06-30 17:00:44 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,97 @@
+﻿#include "Histogram.h"
+
+#include <iostream>
+
+#include "Header.h"
+
+#include <vector>
+
+#include <cmath>
+
+using namespace std;
+
+const size_t SCREEN_WIDTH = 80;
+
+const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+
+vector<double>
+
+input_numbers(size_t count)
+
+{
+
+	vector<double> result(count);
+
+	for (size_t i = 0; i < count; i++)
+
+	{
+
+		cin >> result[i];
+
+	}
+
+	return result;
+
+}
+
+void make_histogram(const vector<double>& numbers, double max, double min, size_t bin_count, vector <size_t>& bins)
+
+{
+
+	for (double x : numbers)
+
+	{
+
+		size_t bin_index = (x - min) / (max - min) * bin_count;
+
+		if (bin_index == bin_count)
+
+		{
+
+			bin_index--;
+
+		}
+
+		bins[bin_index]++;
+
+	}
+
+}
+
+int main()
+
+{
+
+	size_t number_count;
+
+	cerr << "Enter number count:";
+
+	cin >> number_count;
+
+	const auto numbers = input_numbers(number_count);
+
+	size_t bin_count;
+
+	cerr << "Enter bin count:";
+
+	cin >> bin_count;
+
+	size_t scale;
+
+	cerr << "Enter number of scale:";
+
+	cin >> scale;
+
+	vector <size_t> bins(bin_count, 0);
+
+	double min, max;
+
+	find_minmax(numbers, min, max);
+
+	make_histogram(numbers, max, min, bin_count, bins);
+
+	show_histogram_svg(bins, scale);
+
+	return 0;
+
+}
